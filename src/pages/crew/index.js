25228d0570b64e1b544d719d6da516cb2b61e93e
Ilya Shestakov <ilya.shestakov.md@gmail.com>
2023-02-14 15:13:32 +0500

    refactoring before deployment

--- src/pages/crew/index.js
+++ src/pages/crew/index.js
@@ -4,7 +4,7 @@ import Carousel from '../../scripts/components/Carousel';
 import './index.scss';
 
 const header = document.querySelector('.header');
-const crew = document.querySelector('.crew__carousel');
+const crewCarousel = document.querySelector('.crew__carousel');
 
 new Header(header).init();
-new Carousel(crew).init();
+new Carousel(crewCarousel).init();
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,6 +3,12 @@ import Header from '../../scripts/components/Header';
 import './index.scss';
 
 const header = document.querySelector('.header');
+const exploreButton = document.querySelector('.intro__main-button');
 
 new Header(header).init();
 
+exploreButton.addEventListener('click', () => {
+  alert(
+    'You clicked on button "explore", but nothing happened! But something good will happen in your life in the future:)'
+  );
+});
